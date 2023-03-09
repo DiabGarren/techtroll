e3b2c67a881a2b9790ceb869ffb87056ee9a8bb5
DiabGarren <garrendiab@gmail.com>
2023-03-09 16:26:00 +0200

    Only one json

--- js/productData.mjs
+++ js/productData.mjs
@@ -5,4 +5,32 @@ export default class ProductData {
         const data = await res.json();
         return data;
     }
+
+    async getProducts() {
+        const res = await fetch(`../json/products.json`);
+        const data = await res.json();
+        return data;
+    }
+
+    async getProductsByCategory(category) {
+        const productList = await this.getProducts();
+        let products = [];
+        productList.forEach(product => {
+            if (product.Category === category) {
+                products.push(product);
+            }
+        })
+        return products;
+    }
+
+    async getProduct(id) {
+        const productList = await this.getProducts();
+        let item = [];
+        productList.forEach(product => {
+            if (product.Id === id) {
+                item = product;
+            }
+        })
+        return item;
+    }
 }
