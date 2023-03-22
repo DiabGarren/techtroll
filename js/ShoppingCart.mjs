02d8941b155c8f67eeef8203fb549b99c01080fd
DiabGarren <garrendiab@gmail.com>
2023-03-22 19:45:48 +0200

    Removed cart item

--- js/ShoppingCart.mjs
+++ js/ShoppingCart.mjs
@@ -1,24 +1,20 @@
-import { getLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 
 export default class Cart {
     constructor(parentElement) {
-        this.cart = getLocalStorage("cart");
         this.parentElement = parentElement;
     }
-
-    init() {
-        this.renderCart();
-        this.cartTotal();
-    }
-
+    
     renderCart() {
+        const cart = getLocalStorage("cart");
         const wrapper = document.querySelector(this.parentElement);
+        let total = 0;
         let output = `<h2 class="page-header">Cart</h2>
         <div class="product-list">`;
-        if (!this.cart.length) {
-            output += `<h3>Your cart it empty</h3>`;
+        if (!cart.length) {
+            output += `<h3 class="page-header">Your cart is empty</h3>`;
         } else {
-            this.cart.forEach((item) => {
+            cart.forEach((item) => {
                 output +=
                     `<div class="cart-box">
                     <a class="cart_image-container" href="../product/?id=${item.Id}">
@@ -29,25 +25,36 @@ export default class Cart {
                         <p class="cart_price">R${item.Price}</p>
                         <p class="cart_qty">${item.Quantity}</p>
                     </div>
-                    <a class="cart_remove-item">X</a>
+                    <a class="cart_remove-item" id="${item.Id}">X</a>
                 </div>`;
+                total += parseFloat(item.Price) * item.Quantity;
             })
-            output += `</div>`;
+            output += `</div>
+            <div class="cart-total">Total: R`;
+            output += total.toFixed(2);
 
         }
         wrapper.innerHTML = output;
     }
 
-    cartTotal() {
-        const wrapper = document.querySelector(this.parentElement);
-        let output = wrapper.innerHTML;
-        let total = 0;
-
-        output += `<div class="cart-total">Total: R`;
-        this.cart.forEach((item) => {
-            total += parseFloat(item.Price) * item.Quantity;
+}
+export function removeItem(product) {
+    if (product.classList.contains("cart_remove-item")) {
+        const productId = product.id;
+        const cart = getLocalStorage("cart");
+        cart.forEach((item, index) => {
+            if (item.Id === productId) {
+                if (item.Quantity > 1) {
+                    item.Quantity -= 1;
+                } else {
+                    console.log(index);
+                    cart.splice(index, 1);
+                }
+            }
         })
-        output += total.toFixed(2);
-        wrapper.innerHTML = output;
+        setLocalStorage("cart", cart);
+        return true;
+    } else {
+        return false;
     }
 }
--- js/cart.js
+++ js/cart.js
@@ -1,8 +1,19 @@
 import Cart from "./ShoppingCart.mjs";
-import { loadHeaderFooter, loadNavigation } from "./utils.mjs";
+import { loadHeaderFooter, loadNavigation, updateCartIcon } from "./utils.mjs";
+import { removeItem } from "./ShoppingCart.mjs";
 
 loadHeaderFooter(".main-header", ".main-footer");
 loadNavigation(".main-nav");
 
 let cart = new Cart(".cart_wrapper");
-cart.init();
+cart.renderCart();
+
+function removeItemHandler (event) {
+    let removed = removeItem(event.target)
+    if (removed) {
+        cart.renderCart();
+        updateCartIcon();
+    }
+}
+
+document.body.addEventListener('click', removeItemHandler);
