7f0b385bd6f0a79821ad47527d0016b4df7de78f
DiabGarren <garrendiab@gmail.com>
2023-03-22 15:27:35 +0200

    Formatted product page

--- js/ProductPage.mjs
+++ js/ProductPage.mjs
@@ -8,25 +8,24 @@ export default class Product {
         const product = await this.dataScource.getProduct(this.productId);
         return product;
     }
-    
+
     rednerProductPage(item, parentElement) {
         console.log(item);
         const wrapper = document.querySelector(parentElement);
-        let output = `<h2 class="product-category"></h2>
-        <div class="product-list">`;
+        let output = `<h2 class="product-page_title">${item.Name}</h2>
+        <div class="product-page">`;
         output +=
-                `<div class="product-box">
-                    <a class="product-image_container" href="../products/?id=${item.Id}">
-                        <img class="product-image" src="${item.Image}" alt="${item.Name}" />
+            `<a class="product-page_image-container" href="${item.Link}" target="_blank">
+                        <img class="product-page_image" src="${item.Image}" alt="${item.Name}" />
                     </a>
-                    <div class="product-info">
-                        <a class="product-name" href="../products/?id=${item.Id}">${item.Name}</a>
-                        <p class="product-price">R${item.Price}</p>
-                        <p class="product-available">${item.Available}</p>
-                    </div>
-                    <a class="product-btn" href="../products/?id=${item.Id}">View Item</a>
-                </div>`;
-                
+                    <div class="product-page_info">
+                        <h3 class="product-page_name">${item.Name}</h3>
+                        <p class="product-page_price">R${item.Price}</p>
+                        <p class="product-page_available">${item.Available}</p>
+                        <a class="product-page_add-cart-btn">Add to Cart</a>
+                        <a class="product-page_view-item" href="${item.Link}" target="_blank">View Original Page</a>
+                    </div>`;
+
         output += `</div>`;
 
         wrapper.innerHTML = output;
