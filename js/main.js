37071e2400c584138237a49861c8ea28b53c2b0c
DiabGarren <garrendiab@gmail.com>
2023-03-22 16:56:52 +0200

    Format cart if empty

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
-import { loadHeaderFooter, loadNavigation } from "./utils.mjs";
+import { getCartLength, getLocalStorage, loadHeaderFooter, loadNavigation, setLocalStorage } from "./utils.mjs";
 
 loadHeaderFooter(".main-header", ".main-footer");
 loadNavigation(".main-nav");
+
+const cart = getLocalStorage("cart");
+if (!cart) {
+    setLocalStorage("cart", []);
+}
+console.log(getCartLength());
