98c211b690cb795f1469549b06791db99910f059
DiabGarren <garrendiab@gmail.com>
2023-03-30 01:21:22 +0200

    Added search and sort to url

--- js/ProductList.mjs
+++ js/ProductList.mjs
@@ -11,51 +11,86 @@ export default class ProductList {
         return list;
     }
 
-    renderProductList(list, parentElement) {
-        const wrapper = document.querySelector(parentElement);
+    renderTemplate(list, parentElement) {
         let output = `<h2 class="page-header">${this.category[0].toUpperCase()}${this.category.substring(1, this.category.length).replace("-", " ")}</h2>
-        <div class="product-list">`;
+    <div class="product-list">`;
         list.forEach((item) => {
             output +=
                 `<div class="product-box">
-                    <a class="product-list_image-container" href="/techtroll/product/?id=${item.Id}">
-                        <img class="product-list_image" src="${item.ImageSmall}" alt="${item.Name}" />
-                    </a>
-                    <div class="product-list_info">
-                        <a class="product-list_name" href="/techtroll/product/?id=${item.Id}">${item.Name}</a>
-                        <p class="product-list_price"><b>R${formatPrice(item.Price)}</b></p>
-                    </div>
-                    <a class="product-list_btn" href="/techtroll/product/?id=${item.Id}">View Item</a>
-                </div>`;
+                <a class="product-list_image-container" href="/techtroll/product/?id=${item.Id}">
+                    <img class="product-list_image" src="${item.ImageSmall}" alt="${item.Name}" />
+                </a>
+                <div class="product-list_info">
+                    <a class="product-list_name" href="/techtroll/product/?id=${item.Id}">${item.Name}</a>
+                    <p class="product-list_price"><b>R${formatPrice(item.Price)}</b></p>
+                </div>
+                <a class="product-list_btn" href="/techtroll/product/?id=${item.Id}">View Item</a>
+            </div>`;
         })
         output += `</div>`;
 
-        wrapper.innerHTML = output;
+        parentElement.innerHTML = output;
+    }
+
+    renderProductList(list, parentElement, keyword, searchBar, sortDir, sortBtn) {
+        if (sortDir == "desc") {
+            list.reverse();
+            sortBtn.innerHTML = "&#8659;";
+        } else {
+            sortBtn.innerHTML = "&#8657;";
+        }
+        if (keyword) {
+            searchBar.value = keyword;
+            this.search(keyword, list, parentElement);
+        } else {
+            this.renderTemplate(list, parentElement);
+        }
     }
 
     search(keyword, productList, parentElement) {
         const newList = [];
-        if (keyword.length == 0) {
-            this.renderProductList(productList, parentElement);
-        } else {
-            productList.forEach(product => {
-                if (product.Name.toUpperCase().includes(keyword.toUpperCase())) {
-                    newList.push(product);
-                }
-            });
-            
-            if (newList.length == 0) {
-                document.querySelector(parentElement).innerHTML = `<h2 class="page-header">${this.category[0].toUpperCase()}${this.category.substring(1, this.category.length).replace("-", " ")}</h2>
-                <h3 class="page-header">No products found</h3>`;
-            } else {
-                this.renderProductList(newList, parentElement);
+        productList.forEach(product => {
+            if (product.Name.toUpperCase().includes(keyword.toUpperCase())) {
+                newList.push(product);
             }
+        });
+
+        if (newList.length == 0) {
+            parentElement.innerHTML = `<h2 class="page-header">${this.category[0].toUpperCase()}${this.category.substring(1, this.category.length).replace("-", " ")}</h2>
+                <h3 class="page-header">No products found</h3>`;
+        } else {
+            this.renderTemplate(newList, parentElement);
+
         }
         return newList;
+    }
+
+    searchHandler(keyword) {
+        if (keyword.length > 0) {
+            document.location = `/techtroll/product-list/?category=${this.category}&search=${keyword}`;
+        } else {
+            document.location = `/techtroll/product-list/?category=${this.category}`;
+        }
     }
 
     sort(productList, parentElement) {
         productList.reverse();
         this.renderProductList(productList, parentElement);
+    }
+
+    sortHandler(sortDir, keyword) {
+        if (!keyword) {
+            if (!sortDir || sortDir == "asc") {
+                document.location = `/techtroll/product-list/?category=${this.category}&sort=desc`;
+            } else {
+                document.location = `/techtroll/product-list/?category=${this.category}&sort=asc`;
+            }
+        } else {
+            if (!sortDir || sortDir == "asc") {
+                document.location = `/techtroll/product-list/?category=${this.category}&search=${keyword}&sort=desc`;
+            } else {
+                document.location = `/techtroll/product-list/?category=${this.category}&search=${keyword}&sort=asc`;
+            }
+        }
     }
 }
--- js/product-list.js
+++ js/product-list.js
@@ -11,31 +11,27 @@ const category = getParam("category");
 const productList = new ProductList(category, productData);
 const list = await productList.init();
 
-const sortDir = document.querySelector(".sort-dir");
+const wrapper = document.querySelector(".product-list_wrapper");
+
+const sortDir = getParam("sort");
+const sortBtn = document.querySelector(".sort-dir");
+
+const search = getParam("search");
+const searchBar = document.querySelector(".search-bar");
 
 document.title += ` ${category[0].toUpperCase()}${category.substring(1, category.length).replace("-", " ")}`;
-productList.renderProductList(list, ".product-list_wrapper");
+productList.renderProductList(list, wrapper, search, searchBar, sortDir, sortBtn);
 
-const searchBar = document.querySelector(".search-bar");
 const searchBtn = document.querySelector(".search-btn");
 let searchedList = [];
 
 searchBar.addEventListener("change", (event) => {
-    searchedList = productList.search(event.target.value, list, ".product-list_wrapper");
+    searchedList = productList.searchHandler(event.target.value);
 })
 searchBtn.addEventListener("click", () => {
-    searchedList = productList.search(searchBar.value, list, ".product-list_wrapper");
+    searchedList = productList.searchHandler(searchBar.value);
 })
 
-sortDir.addEventListener("click", (event) => {
-    if (event.target.textContent == "&#8657;" || event.target.textContent == "⇑") {
-        event.target.innerHTML = "&#8659;";
-    } else {
-        event.target.innerHTML = "&#8657;";
-    }
-    if (searchedList.length == 0) {
-        productList.sort(list, ".product-list_wrapper");
-    } else {
-        productList.sort(searchedList, ".product-list_wrapper");
-    }
+sortBtn.addEventListener("click", () => {
+    productList.sortHandler(sortDir, search);
 })
